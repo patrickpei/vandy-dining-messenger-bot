94b86fb4882d67364eeba0d592412ec1d716db74
Patrick <patrickpei@users.noreply.github.com>
2017-12-02 15:22:31 -0600

    Port over pub work

---
+++ index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const
+    bodyParser = require('body-parser'),
+    checkOrders = require('./orders'),
+    configureRoutes = require('./routes'),
+    express = require('express'),
+    morgan = require('morgan');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(morgan('combined'));
+configureRoutes(app);
+
+// setInterval(checkOrders, 10000);
+
+app.listen(process.env.PORT || 1337, () => console.log('[webhook]: listening'));
---
+++ routes.js
@@ -0,0 +1,73 @@
+const fetch = require('node-fetch');
+const request = require('superagent');
+
+let orderNumbers = [461, 462, 479, 481, 482, 483, 487, 488, 499, 501];
+
+let configureRoutes = app => {
+    app.get('/', (req, res) => {
+        res.status(200).send('Welcome to The Pub At Overcup Oak.');
+    })
+    
+    app.get('/webhook', (req, res) => {
+        const VERIFY_TOKEN = 'WC4y3U^JV@1Ehd^g';
+        const mode = req.query['hub.mode'];
+        const token = req.query['hub.verify_token'];
+        const challenge = req.query['hub.challenge'];
+
+        if (mode && token) {
+            // Checks the mode and token sent is correct
+            if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+                console.log('[webhook]: verified');
+                res.status(200).send(challenge);
+            } else {
+                res.sendStatus(403);
+            }
+        }
+    });
+    
+    app.post('/webhook', (req, res) => {
+        let body = req.body;
+    
+        // Page subscriptions only
+        if (body.object === 'page') {
+            body.entry.forEach(function(entry) {
+                // Gets the message. entry.messaging is arr, but len = 1 so index 0
+                let webhookEvent = entry.messaging[0];
+                console.log('[webhook]: event: ', webhookEvent);
+            });
+    
+            res.status(200).send('EVENT_RECEIVED');
+        } else {
+            res.sendStatus(404);
+        }
+    });
+
+    app.get('/orders', (req, res) => {
+        // const pubLink = 'http://campusdining.vanderbilt.edu/?action=cmi_yoir&request=screen&location_id=752';
+        const pubLink = 'http://127.0.0.1:1337/fakeorders';
+
+        request.get(pubLink)
+            .end((err, pubRes) => {
+                res.status(200).send(pubRes.body);
+            });
+
+        // Who made this incompatible with non absolute urls...
+        // fetch(pubLink)
+        //     .then(pubRes => {
+        //         // console.log(pubRes);
+        //         res.status(200).send(pubRes);
+        //     });
+    });
+
+    app.get('/fakeorders', (req, res) => {
+        res.status(200).send(numbers);
+    });
+
+    app.put('/fakeorders', (req, res) => {
+        console.log('b', req.body);
+    
+        res.status(200).send('hi');
+    })
+}
+
+module.exports = configureRoutes;
