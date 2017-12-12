099dd2cfef04a66728bf8e1784cb7dee85dd319a
Melvin Lu <melvinlu1996@gmail.com>
2017-12-11 22:43:20 -0600

    Finish menu retrieval

--- routes.js
+++ routes.js
@@ -3,6 +3,10 @@ const superagent = require('superagent');
 const rp = require('request-promise');
 const cheerio = require('cheerio');
 
+String.prototype.capitalize = function() {
+    return this.replace(/(?:^|\s)\S/g, function(a) { return a.toUpperCase(); });
+};
+
 const restaurants = [
     'Bamboo Bistro',
     'Food for Thought Cafe',
@@ -31,20 +35,18 @@ const restaurants = [
     'Soup Commons',
     'Wok Station',
     'Grins Vegetarian Cafe',
-    'The Kitchen',
-    'Rocket Subs',
+    'Kissam (The Kitchen)',
     'Pi & Leaf',
     'Local Java',
     'The Pub'
 ];
 const welcomeMessage = 'Welcome to Vanderbilt Dining Experience (VDE)!\n\n' +
                        'Available commands:\n\n' +
-                       '`menu help`: get names of available ' +
-                       'restaurants\n\n' +
+                       '`menu list`: get list of available restaurants\n\n' +
+                       '`menu open`: get list of currently open restaurants`' +
+                       `\n\n` +
                        '`menu <restaurant>`: get the current menu for ' +
-                       'the specified restaurant (can be lowercase, but ' +
-                       'must match one from the list returned by ' +
-                       '`menu help`)\n\n' +
+                       'the specified restaurant\n\n' +
                        'Send any other text to receive this message again.' +
                        '\n\nHelp us get better!: ' +
                        'https://www.facebook.com/vandydiningexp';
@@ -67,11 +69,13 @@ let configureRoutes = app => {
         }
 
         const entries = requestBody.entry;
-        entries.forEach(entry => {
+        entries.forEach(async entry => {
             // Gets the message. only ever 1 element in entry.messaging (Array)
             console.log('entry: ', JSON.stringify(entry));
             const message = entry.messaging[0];
-            const url = `https://graph.facebook.com/v2.6/me/messages?access_token=${process.env.access_token}`;
+            const url =
+                `https://graph.facebook.com/v2.6/me/messages?access_token=` +
+                `${process.env.access_token}`;
             let body = {
                 'messaging_type': 'RESPONSE',
                 'recipient': {
@@ -85,8 +89,42 @@ let configureRoutes = app => {
             let text = message.message.text;
             if (text !== undefined) {
                 text = text.toLowerCase();
-
-                if (text.includes('pub')) {
+                if (text.includes('menu')) {
+                    if (text == 'menu list') {
+                        let menu_list =
+                            'Here is the list of available restaurants ' +
+                            'to query:\n';
+                        restaurants.forEach((restaurant) => {
+                            menu_list += (restaurant + '\n');
+                        });
+                        body.message.text =
+                            menu_list.substring(0, menu_list.length - 1);
+                    } else if (text == 'menu open') {
+                        let open_restaurants =
+                            'Here is the list of currently open restaurants ' +
+                            'to query:\n';
+                        let open_places = await getOpenRestaurants();
+                        body.message.text = open_places.join('\n');
+                    } else {
+                        let restaurant = text.substring(5).capitalize();
+                        if (restaurant == 'Food For Thought Cafe') {
+                            restaurant[5] = 'f';
+                        }
+                        if (restaurant == 'Kissam') {
+                            restaurant = "The Kitchen";
+                        }
+                        console.log('Getting menu for ' + restaurant + '.');
+                        let menu_items = await getMenuItems(restaurant);
+                        let menu_items_text = menu_items.join('\n');
+                        body.message.text = menu_items_text.length == 0 ||
+                                            menu_items_text.length > 640
+                            ? `Sorry, for one reason or another, I'm unable ` +
+                              `to provide you the menu for ${restaurant} at ` +
+                              `this time. Working on a fix!`
+                            : menu_items_text;
+                        console.log('Sent menu items back to user.');
+                    }
+                } else if (text.includes('pub')) {
                     const match = text.match(/\d+/g);
                     if (match === null) {
                         body.message.text =
@@ -99,19 +137,6 @@ let configureRoutes = app => {
                         const num = match[0];
                         body.message.text =
                             `Tracking Pub Order #${num} for you.`;
-                    }
-                } else if (text.includes('menu')) {
-                    if (text == 'menu help') {
-                        let menu_list =
-                            'Here is the list of available restaurants ' +
-                            'to query:\n';
-                        restaurants.forEach((restaurant) => {
-                            menu_list += (restaurant + '\n');
-                        });
-                        body.message.text =
-                            menu_list.substring(0, menu_list.length - 1);
-                    } else {
-                        body.message.text = welcomeMessage;
                     }
                 } else {
                     body.message.text = welcomeMessage;
@@ -154,39 +179,52 @@ let configureRoutes = app => {
             res.sendStatus(500);
         }
     });
+}
 
-    app.get('/menus', (req, res) => {
-        const options = {
-            url: 'http://vanderbilt.mymenumanager.net/',
-            transform: function (body) {
-                return cheerio.load(body);
-            }
-        };
-
-        rp(options)
+async function getOpenRestaurants() {
+    const options = {
+        url: 'http://vanderbilt.mymenumanager.net/',
+        transform: function (body) {
+            return cheerio.load(body);
+        }
+    };
+    return rp(options)
             .then(($) => {
-                let user_request = 'The Kitchen';
-                let restaurant = $('.menu_block').find(`div.concept_name:contains(${user_request})`);
-                console.log(restaurant.parent().html());
-                // console.log(restaurant.parent().find('li').each(
-                //     () => {
-                //         console.log($(this).text())
-                //     }
-                // ));
-                // let menu = [];
-                // $('.menu_block').eq(0).find('li').each(
-                //     () => {
-                //         menu.push($(this).text())
-                //     });
-                // menu.forEach((item) => {
-                //     console.log(item);
-                // });
-                res.status(200).send('Done.');
+                let open_restaurants =[];
+                let open_concepts = $('.menu_block')
+                        .find('div.concept_name');
+                for (var i = 0; i < open_concepts.length; ++i) {
+                    open_restaurants.push($(open_concepts[i]).text());
+                }
+                return open_restaurants;
             })
             .catch((err) => {
                 console.log(err);
             });
-    });
+}
+
+async function getMenuItems(restaurant) {
+    const options = {
+        url: 'http://vanderbilt.mymenumanager.net/',
+        transform: function (body) {
+            return cheerio.load(body);
+        }
+    };
+    return rp(options)
+            .then(($) => {
+                let menu = [];
+                let menu_items = $('.menu_block')
+                        .find(`div.concept_name:contains(${restaurant})`)
+                        .closest('.menu_block')
+                        .find('li');
+                for (var i = 0; i < menu_items.length; ++i) {
+                    menu.push($(menu_items[i]).text());
+                }
+                return menu;
+            })
+            .catch((err) => {
+                console.log(err);
+            });
 }
 
 function getBase(req, res) {
@@ -206,7 +244,9 @@ function getBase(req, res) {
 }
 
 async function getOrders(req, res) {
-    const url = 'http://campusdining.vanderbilt.edu/?action=cmi_yoir&request=orderqueue_ajax&location_id=752';
+    const url =
+        'http://campusdining.vanderbilt.edu/?action=cmi_yoir&request' +
+        '=orderqueue_ajax&location_id=752';
     const getOrders = () => {
         return fetch(url)
             .then(res => res.text())
